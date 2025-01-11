8706411c852d1d2878f307bd4320ac54d7a8b09e
Denian <denianszz@gmail.com>
2025-01-11 19:51:26 -0300

    feat: adding migration for user

--- src/Backend/IngrEasy.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/IngrEasy.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,6 @@
-﻿using IngrEasy.Domain;
+﻿using System.Reflection;
+using FluentMigrator.Runner;
+using IngrEasy.Domain;
 using IngrEasy.Domain.Repositories.User;
 using IngrEasy.Infrastructure.DataAcess;
 using IngrEasy.Infrastructure.DataAcess.Repositories;
@@ -11,25 +13,39 @@ namespace IngrEasy.Infrastructure;
 
 public static class DependencyInjectionExtension
 {
-    public  static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext(configuration);   
+        services.AddDbContext(configuration);
         services.AddRepositories();
-        
+        services.AddFluentMigrator(configuration);
+
     }
 
     private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<IngrEasyDbContext>(options =>
-        { 
+        {
             options.UseMySQL(configuration.AddConnectionString()!);
         });
     }
-    
+
     private static void AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
         services.AddScoped<IUserReadOnlyRepository, UserRepository>();
     }
+
+    private static void AddFluentMigrator(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddFluentMigratorCore().ConfigureRunner(opt =>
+        {
+            opt.AddMySql8()
+                .WithGlobalConnectionString(connectionString).ScanIn(Assembly.Load("IngrEasy.Infrastructure")).For.All();
+        });
+    }
+
 }
---
+++ src/Backend/IngrEasy.Infrastructure/Migrations/Version0001.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using IngrEasy.Infrastructure.Migrations.Versions;
+
+namespace IngrEasy.Infrastructure.Migrations;
+[Migration(DatabaseVersion.TableUser, "Create the User table")]
+
+public class Version0001 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Users")
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("Email").AsString(255).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
+    }
+}
