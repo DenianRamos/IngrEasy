e95b2af4f6abe58350469c1315d4ec59a0e59271
Denian <denianszz@gmail.com>
2025-01-17 13:48:44 -0300

    feat: add test unit for validate

--- src/Backend/IngrEasy.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/IngrEasy.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -11,8 +11,13 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         RuleFor(x => x.Name).NotEmpty().WithMessage(ResourceErrorMessage.NAME_EMPTY);
         RuleFor(x => x.Email).NotEmpty().WithMessage(ResourceErrorMessage.EMAIL_EMPTY);
-        RuleFor(x => x.Email).EmailAddress().WithMessage(ResourceErrorMessage.EMAIL_INVALID);
-
+        When(user =>
+                string.IsNullOrEmpty(user.Email) == false, () =>
+            { 
+                RuleFor(x => x.Email).EmailAddress().WithMessage(ResourceErrorMessage.EMAIL_INVALID);
+            }
+        );
         RuleFor(x => x.Password).NotEmpty().WithMessage(ResourceErrorMessage.PASSWORD_EMPTY);
+        RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceErrorMessage.PASSWORD_INVALID);
     }
 }
---
+++ tests/Validator.tests/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using IngrEasy.Application.UseCases.User.Register;
+using IngrEasy.Exception;
+
+namespace Validator.tests.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Sucess()
+        {
+             var validator = new RegisterUserValidator();
+             var request = RequestRegisterUserJsonBuilder.Build();
+             var result = validator.Validate(request); 
+             result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Error_Name_Empty()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = String.Empty;
+            
+            var result = validator.Validate(request); 
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessage.NAME_EMPTY));
+        }
+        
+        
+        [Fact]
+        public void Error_Email_Invalid()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = ".com";
+            
+            var result = validator.Validate(request); 
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessage.EMAIL_INVALID));
+        }
+        
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Error_Password_Invalid(int passwordLenght)
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build(passwordLenght);
+            var result = validator.Validate(request); 
+            
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle()
+                .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessage.PASSWORD_INVALID));
+            
+    }
+        }
+}
