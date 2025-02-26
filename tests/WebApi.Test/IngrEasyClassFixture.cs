e995636502b68c00db34e2c9066265cd02c6f08a
Denian <denianszz@gmail.com>
2025-02-26 14:46:46 -0300

    feat: adding unit test for login

---
+++ tests/WebApi.Test/IngrEasyClassFixture.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+
+namespace WebApi.Test;
+
+public class IngrEasyClassFixture : IClassFixture<CustomWebApplicationFactory>
+
+{
+    
+    private readonly HttpClient _client;
+
+    public IngrEasyClassFixture(CustomWebApplicationFactory factory) => _client =  factory.CreateClient();
+
+    protected async Task<HttpResponseMessage> DoPost(string method, object request, string culture = "en")
+    {
+        ChangeRequestCulture(culture);
+        return await _client.PostAsJsonAsync(method, request);
+
+    }
+
+    private void ChangeRequestCulture(string culture)
+    {
+        if(_client.DefaultRequestHeaders.Contains("Accept-Language"))
+            _client.DefaultRequestHeaders.Remove("Accept-Language");
+        
+        _client.DefaultRequestHeaders.Add("Accept-Language", culture);
+    }
+
+}
---
+++ tests/WebApi.Test/Login/DoLoginTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using IngrEasy.Communication.Requests;
+using IngrEasy.Exception;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Login;
+
+public class DoLoginTest : IngrEasyClassFixture
+{
+    private readonly string _method = "login";
+    
+    
+    private readonly string _email;
+    
+    private readonly string _password;
+    
+    private readonly string _name;
+
+
+    public DoLoginTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _email = factory.GetEmail();
+        _password = factory.GetPassword();
+        _name = factory.GetName();
+        
+
+    }
+
+    [Fact]
+    public async Task Sucess()
+    {
+        var request = new RequestLoginJson{Email = _email, Password = _password};
+                
+        var response = await DoPost(_method, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        await using var responseBoby = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBoby);
+        
+        responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(_name);
+        
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+
+    public async Task Error_Invalid_user(string culture)
+    {
+        var request = RequestLoginJsonBuilder.Build();
+        
+
+        
+        var response = await  DoPost(_method, request,culture);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        
+        await using var responseBoby = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBoby);
+
+        var erros = responseData.RootElement.GetProperty("errors").EnumerateArray().Should();
+        
+        var expectedMessage = ResourceErrorMessage.ResourceManager.GetString("EMAIL_OR_PASSWORD_INVALID", new System.Globalization.CultureInfo(culture));
+        erros.ContainSingle().And.Contain(error => error.GetString()!.Equals(expectedMessage));
+    }
+    
+    
+    
+    
+    
+}
